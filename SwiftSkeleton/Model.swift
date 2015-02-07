8f68a5348a20705b76956bab2feb573462b1119f
wilzh40 <wilzh40@gmail.com>
2015-02-07 13:10:08 -0800

    Added a track model

--- SwiftSkeleton/Model.swift
+++ SwiftSkeleton/Model.swift
@@ -10,3 +10,21 @@ import Foundation
 
 
 
+class Track: NSObject {
+    
+    // Mandatory Variables
+    var title: String
+    var id: Int
+    var duration: Int
+    var stream_url: String
+    var permalink_url: String
+    
+    // Optional Variables (could be nil if not there)
+    var genre: String?
+    var description: String?
+    var artwork_url: String?
+    
+    
+}
+
+class s
